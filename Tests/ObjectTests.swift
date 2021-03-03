a9125470d451ca18d1277805b6c8cc9e20200376
Helbert Gomes <helbertgs@gmail.com>
2021-03-02 21:35:07 -0300

    chore: update Github Actions

---
+++ Tests/ObjectTests.swift
@@ -0,0 +1,16 @@
+import Nimble
+import Quick
+import XCTest
+@testable import Jarvis
+
+final class ObjectTests: QuickSpec {
+    override func spec() {
+        describe("Object") {
+            context("") {
+                it("") {
+
+                }
+            }
+        }
+    }
+}
